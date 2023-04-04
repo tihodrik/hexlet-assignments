c0046ef032a3d68c0b9fa4bb32a29c8168d4757a
Dasha Tikhomirova <Tikhomirova.DG@tversu.ru>
2023-04-04 15:34:09 +0300

    gems added, rubocop configured

---
+++ rails-basics-ru/quality/examples/app/controllers/notes_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all.order(created_at: :desc)
+  end
+
+  def show
+    # debugger
+    @note = Note.find(params[:id])
+  end
+end
