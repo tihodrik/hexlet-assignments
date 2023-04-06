24f6eb75deea05a692f0c177b8128e44aec60c65
@hexlet/cli <support@hexlet.io>
2023-04-06 09:40:13 +0300

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/app/models/task.rb
+++ rails-basics-ru/crud/app/models/task.rb
@@ -1,19 +1,4 @@
 # frozen_string_literal: true
-
-# == Schema Information
-#
-# Table name: tasks
-#
-#  id          :integer          not null, primary key
-#  completed   :boolean          default(FALSE), not null
-#  creator     :string           not null
-#  description :text
-#  name        :string           not null
-#  performer   :string
-#  status      :string           default("new"), not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#
 class Task < ApplicationRecord
   validates :name, :status, :creator, presence: true
   validates :completed, inclusion: [true, false]
